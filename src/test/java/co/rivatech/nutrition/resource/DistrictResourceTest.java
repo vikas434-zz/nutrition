124f1486ff9aabc45ed75f4fc9fe5b4baf869827
Vikas Ranjan <vranjan@groupon.com>
2021-07-29 00:42:17 +0530

    PMD check added

---
+++ src/test/java/co/rivatech/nutrition/resource/DistrictResourceTest.java
@@ -0,0 +1,17 @@
+package co.rivatech.nutrition.resource;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author vranjan
+ * @created 29/07/2021
+ */
+class DistrictResourceTest {
+
+    @Test
+    void getAllDistrict() {
+
+    }
+}
