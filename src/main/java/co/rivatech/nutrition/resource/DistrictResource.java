d7f0fee2d6a4d924cd0b6cad581caf2915e6b7db
Vikas Ranjan <vranjan@groupon.com>
2021-08-07 23:42:15 +0530

    District resource add and get districts

--- src/main/java/co/rivatech/nutrition/resource/DistrictResource.java
+++ src/main/java/co/rivatech/nutrition/resource/DistrictResource.java
@@ -1,13 +1,19 @@
 package co.rivatech.nutrition.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import co.rivatech.nutrition.model.District;
+import co.rivatech.nutrition.service.DistrictService;
 import io.swagger.annotations.ApiOperation;
 
 /**
@@ -18,10 +24,21 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/v1/district")
 public class DistrictResource {
 
+    //TODO TDD based validations and exceptions
+
+    @Autowired
+    private DistrictService districtService;
+
     @GetMapping("/")
     @ApiOperation(value = "Get lists of all the district")
     public List<District> getAllDistrict() {
-        return new ArrayList<>();
+       return districtService.getAllDistricts();
+    }
+
+    @PostMapping("/add")
+    @ApiOperation(value = "Add a district")
+    public ResponseEntity<District> addDistrict(@Nonnull @RequestBody District district) {
+        return ResponseEntity.ok(districtService.addDistrict(district));
     }
 
 }
---
+++ src/main/java/co/rivatech/nutrition/service/DistrictService.java
@@ -0,0 +1,34 @@
+package co.rivatech.nutrition.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.rivatech.nutrition.model.District;
+import co.rivatech.nutrition.repository.DistrictRepository;
+
+/**
+ * @author vranjan
+ * created 07/08/2021
+ */
+@Service
+public class DistrictService {
+
+    @Autowired
+    private DistrictRepository districtRepository;
+
+
+    public List<District> getAllDistricts() {
+        List<District> districts = new ArrayList<>();
+        Iterable<District> items = districtRepository.findAll();
+        items.forEach(districts::add);
+        return districts;
+    }
+
+    public District addDistrict(final District district) {
+        districtRepository.save(district);
+        return district;
+    }
+}
