93ddef1007f9007d753ca99d3b49405ee84efdf3
Vikas Ranjan <vranjan@groupon.com>
2021-09-22 18:49:00 +0530

    Question title added

--- src/main/java/co/rivatech/nutrition/model/LMSQnA.java
+++ src/main/java/co/rivatech/nutrition/model/LMSQnA.java
@@ -26,12 +26,19 @@ public class LMSQnA {
     private int id;
 
     @Column(name = "lms_id")
-    @ApiModelProperty(value = "lms id", required = true)
+    @ApiModelProperty(value = "lms id",
+                      required = true)
     private int lmsId;
 
     @Column(name = "question_no")
-    @ApiModelProperty(value = "Question no.", required = true)
+    @ApiModelProperty(value = "Question no.",
+                      required = true)
     private int questionNo;
+
+    @Column(name = "question_title")
+    @ApiModelProperty(value = "Question title",
+                      required = true)
+    private String questionTitle;
 
     @Type(type = "jsonb")
     @Column(name = "options",
@@ -41,11 +48,13 @@ public class LMSQnA {
     private Options options;
 
     @Column(name = "right_option")
-    @ApiModelProperty(value = "Right option for the question", required = true)
+    @ApiModelProperty(value = "Right option for the question",
+                      required = true)
     private int rightOption;
 
     @Column(name = "marks")
-    @ApiModelProperty(value = "maximum marks of the question", required = true)
+    @ApiModelProperty(value = "maximum marks of the question",
+                      required = true)
     private int marks;
 
     @Column(name = "created_at")
