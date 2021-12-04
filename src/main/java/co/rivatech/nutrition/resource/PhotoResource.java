daabe4f3b60b0800f53923ccf60ef03b07219764
Vikas Ranjan <vranjan@groupon.com>
2021-12-04 09:08:38 +0530

    Get photos by block

--- src/main/java/co/rivatech/nutrition/resource/PhotoResource.java
+++ src/main/java/co/rivatech/nutrition/resource/PhotoResource.java
@@ -1,14 +1,22 @@
 package co.rivatech.nutrition.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
 import co.rivatech.nutrition.dto.UploadImageResponse;
 import co.rivatech.nutrition.enums.MEALTYPE;
+import co.rivatech.nutrition.model.User;
 import co.rivatech.nutrition.service.AWSS3Service;
 import io.swagger.annotations.ApiOperation;
 
@@ -37,5 +45,11 @@ public class PhotoResource {
     public UploadImageResponse uploadPhotoAfterMeal(@RequestParam(value = "file") MultipartFile file,
                                                     @RequestParam(value = "tolaId") int tolaId) {
         return awss3Service.uploadFile(file, tolaId, MEALTYPE.AFTER_MEAL);
+    }
+
+    @GetMapping("/getPhotosByBlock/{blockId}")
+    @ApiOperation(value = "[WORK IN PROGRESS] Get all photos, tied to that block. ")
+    public List<String> getListOfPhotosForBlock(@Nonnull @PathVariable int blockId) {
+        return Collections.emptyList();
     }
 }
